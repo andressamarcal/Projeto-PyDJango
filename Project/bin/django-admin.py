bc1b4bd2d7fb89a4e4e26f97ca6c3ae1edc94aef
Andressa Marçal <andressagb@gmail.com>
2017-10-23 10:17:13 -0300

    Pacotes e Dependencias do Projeto

---
+++ Project/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/andressamarcal/Documentos/WelcomeToTheDjango/Project/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
